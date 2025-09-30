10c4afed9543d54cac07517c69516b67e91eada4
Shernatpro <shernatpro@gmail.com>
2025-09-30 10:12:27 +0300

    Simplify Metro config to fix cache error

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,5 @@
 const { getDefaultConfig } = require('@expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-config.resolver.nodeModulesPaths = [path.resolve(__dirname, 'node_modules')];
-config.resolver.extraNodeModules = {
-  'react-native-web': path.resolve(__dirname, 'node_modules/react-native-web'),
-  'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-  'react': path.resolve(__dirname, 'node_modules/react'),
-};
-
-config.resolver.assetExts.push('cjs');
-config.resolver.sourceExts.push('mjs');
-config.resolver.unstable_conditionNames = ['require', 'import', 'react-native'];
 
 module.exports = config;
