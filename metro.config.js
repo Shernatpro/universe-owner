ca11a0e2e4d744e3c1203d455f638a69b1c72d9d
Shernatpro <shernatpro@gmail.com>
2025-09-30 10:01:39 +0300

    Added metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  'react-native-web': path.resolve(__dirname, 'node_modules/react-native-web'),
+  'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+  'react': path.resolve(__dirname, 'node_modules/react'),
+};
+
+config.resolver.assetExts.push('cjs');
+config.resolver.sourceExts.push('mjs');
+
+module.exports = config;
